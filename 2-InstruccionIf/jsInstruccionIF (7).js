1aadbdba4dc5f26b179587a48fd525ded98a4454
JuanRamos95 <juan09_14@live.com>
2017-02-08 11:53:55 -0300

    ejerc7 terminado

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,14 @@
 function Mostrar()
 {
-//tomo la edad  
+var edad;
+var estadoCivil;
+ edad=document.getElementById('edad').value;
+ edad=parseInt(edad);
+ estadoCivil=document.getElementById('estadoCivil').value;
+ if(edad<18 && estadoCivil !="soltero")
+    {
+ 	    document.write("es pequeño para ser soltero");
+    }
 
-	
-
-
+ 	
 }//FIN DE LA FUNCIÓN
