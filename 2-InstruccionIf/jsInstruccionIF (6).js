7ac2dc77469337b8c40969bf4f7319157b9ca9a6
JuanRamos95 <juan09_14@live.com>
2017-02-08 11:13:54 -0300

    ejerc6 terminado

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,19 @@
 function Mostrar()
 {
-//tomo la edad  
-
-
-
+var edad;
+ edad=document.getElementById('edad').value;
+ edad=parseInt(edad);
+ if(edad>=13 && edad <=17)
+    {
+ 	    alert("es adolecente");
+    }
+if (edad<=12)
+ 	{
+ 		alert("Es menor");
+ 	}
+ if (edad>=18)
+ 	{
+ 		alert("Es mayor de edad");
+ 	}  
 
 }//FIN DE LA FUNCIÓN
