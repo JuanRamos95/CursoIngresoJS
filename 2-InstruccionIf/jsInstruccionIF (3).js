1efb5467941ea6dcb145211db492d2bd284127fc
JuanRamos95 <juan09_14@live.com>
2017-02-08 09:54:01 -0300

    EJERC 3 TERMINADO

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
-//tomo la edad  
-
+var edad;
+ edad=document.getElementById('edad').value;
+ edad=parseInt(edad);
+ if (edad>=18){
+ 	alert("ES MAYOR DE EDAD");
+    }
+ 	else
+ 	{
+ 		alert("es menor de edad");
+ 	}
 
 }//FIN DE LA FUNCIÓN
