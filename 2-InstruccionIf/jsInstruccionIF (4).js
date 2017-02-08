03a61e47815166346156c97e29eb253820590acf
JuanRamos95 <juan09_14@live.com>
2017-02-08 10:32:24 -0300

    ejerc4 TERMINADO

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,20 @@
 function Mostrar()
 {
-//tomo la edad  
+var edad;
+ edad=document.getElementById('edad').value;
+ edad=parseInt(edad);
+ if (edad>=13 && edad <=17)
+    {
+ 	alert("Es adolecente");
+    }
+if (edad<=12)
+{
+	alert("Es menor");
 
-
-
-}//FIN DE LA FUNCIÓN
+ 	}
+ 	if (edad>=18)
+ 	{
+ 		alert("Es mayor de edad");
+ 	}
+ 	
+}
