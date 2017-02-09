ff29f249afdb1da1925b97b05b48f882e6b8c3b4
JuanRamos95 <juan09_14@live.com>
2017-02-09 11:41:36 -0300

    ejerc3 terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -4,8 +4,27 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
+switch(mesDelAño)
+{
 
+	case "Febrero":
+	alert("Este mes no tiene mas de 29 dias");
+	break;
+
+	case "Enero":
+	case "Marzo":
+	case "Abril":
+    case "Mayo":
+    case "Junio":
+	case "Julio":
+    case "Agosto":
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+	case "Diciembre":
+	alert("Este mes tiene 30 o mas dias");
+	break;
+
+}
 
 }//FIN DE LA FUNCIÓN
