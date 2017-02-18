70239bab27f076f7e7d641d94f35d86c23e09e8f
JuanRamos95 <juan09_14@live.com>
2017-02-17 23:32:53 -0300

    Ejerc4 term

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,18 @@
 function Mostrar()
-{
-
+{   
+    var numero=0;
 	var numero = prompt("ingrese un número entre 0 y 10.");
+    numero=parseInt(numero);
+    	while(numero <0 || numero >10)
+    	{
+    		numero=prompt("Error vuelva a ingresar numero");
+    		
+    	}
+    	if(numero >=0 && numero <9)
+        {
+        	alert("Numero correcto ingresado");
+        }
 
+        document.getElementById('numero').value=numero;
 
 }//FIN DE LA FUNCIÓN
