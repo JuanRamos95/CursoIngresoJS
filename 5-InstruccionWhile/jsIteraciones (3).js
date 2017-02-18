99693ff37c088d11d7d22642b3301f4bcad0b019
JuanRamos95 <juan09_14@live.com>
2017-02-17 22:58:56 -0300

    Ejerc3 term con cambios en la clase y mios.

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,20 @@
 function Mostrar()
 {
+     var intentos=0;
+     var clave = prompt("ingrese el número clave.");
 
-var clave = prompt("ingrese el número clave.");
-
-
-}//FIN DE LA FUNCIÓN
+      while(clave != "utn750"  &&  intentos <3)
+      {
+	    clave=prompt("ingrese el numero clave");
+        intentos++;
+      }
+      if (clave =="utn750")
+      {
+        alert("clave correcta")
+      } 
+      else(intentos ==4)
+      {
+      	alert("la cuenta supero el maximo de intentos disponibles, se bloqueara");
+      }
+      
+}//FIN DE LA FUNCIÓN / Luego le agregamos un contador para los intentos y yo un mensaje de advertenciaal final para el bloqueo.
